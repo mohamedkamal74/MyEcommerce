3177cf90fd54373b9653565e390b73400295b604
mohamed kamal <mkahf5103@gmail.com>
2022-09-05 20:57:02 +0400

    some changes in design

--- MyEcommerce/Controllers/HomeController.cs
+++ MyEcommerce/Controllers/HomeController.cs
@@ -18,7 +18,17 @@ namespace MyEcommerce.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
         {
             return View();
         }
