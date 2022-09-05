0069867b8fbe323a17485d965ae90401721d5f57
mohamed kamal <mkahf5103@gmail.com>
2022-09-05 23:59:11 +0400

    add admin area

--- MyEcommerce/Program.cs
+++ MyEcommerce/Program.cs
@@ -39,6 +39,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+app.MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
