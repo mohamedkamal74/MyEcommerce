e2a50d9d47eaa06fc5565ae51cabfa2dcbf4101f
mohamed kamal <mkahf5103@gmail.com>
2022-09-06 03:08:00 +0400

    add create and edit views

---
+++ MyEcommerce/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEcommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
