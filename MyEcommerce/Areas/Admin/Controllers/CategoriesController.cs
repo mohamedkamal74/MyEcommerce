7c45c32a20a4fb018b62b27532820fefc78c51ce
mohamed kamal <mkahf5103@gmail.com>
2022-09-06 04:16:10 +0400

    add create post

--- MyEcommerce/Areas/Admin/Controllers/CategoriesController.cs
+++ MyEcommerce/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,19 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyEcommerce.Data;
+using MyEcommerce.Models;
 
 namespace MyEcommerce.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoriesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Categories.ToList());
         }
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Create(Category model,IFormFile File)
+        {
+            if (ModelState.IsValid)
+            {
+                if (File != null)
+                {
+                    string imagename = Guid.NewGuid().ToString() + ".jpg";
+                    string filepath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/category", imagename);
+
+                    using (var filestream = System.IO.File.Create(filepath))
+                    {
+                        await File.CopyToAsync(filestream);
+
+                    }
+                    model.CategoryPhoto = imagename;
+                }
+                _context.Categories.Add(model);
+                 await  _context.SaveChangesAsync();
+             return   RedirectToAction(nameof(Index));
+            }
+              
+            return View(model);
+
+        }
+
         [HttpGet]
         public IActionResult Edit()
         {
