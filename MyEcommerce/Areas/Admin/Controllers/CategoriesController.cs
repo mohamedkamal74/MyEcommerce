4519be9c019e6a62cce575a19c956d2ed4a52e90
mohamed kamal <mkahf5103@gmail.com>
2022-09-07 02:48:44 +0400

    add edit,delete action and views

--- MyEcommerce/Areas/Admin/Controllers/CategoriesController.cs
+++ MyEcommerce/Areas/Admin/Controllers/CategoriesController.cs
@@ -25,7 +25,7 @@ namespace MyEcommerce.Areas.Admin.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task <IActionResult> Create(Category model,IFormFile File)
+        public async Task<IActionResult> Create(Category model, IFormFile File)
         {
             if (ModelState.IsValid)
             {
@@ -42,18 +42,74 @@ namespace MyEcommerce.Areas.Admin.Controllers
                     model.CategoryPhoto = imagename;
                 }
                 _context.Categories.Add(model);
-                 await  _context.SaveChangesAsync();
-             return   RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-              
+
             return View(model);
 
         }
 
         [HttpGet]
-        public IActionResult Edit()
+        public IActionResult Edit(int? Id)
         {
-            return View();
+            var category = _context.Categories.Find(Id);
+            if (category == null)
+                return NotFound();
+            else
+                return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category model, IFormFile File)
+        {
+            if (ModelState.IsValid)
+            {
+                if (File != null)
+                {
+                    string imagename = Guid.NewGuid().ToString() + ".jpg";
+                    string filepath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot/category", imagename);
+
+                    using (var filestream = System.IO.File.Create(filepath))
+                    {
+                        await File.CopyToAsync(filestream);
+
+                    }
+                    model.CategoryPhoto = imagename;
+                }
+                else
+                {
+                    model.CategoryPhoto = model.CategoryPhoto;
+                }
+                _context.Categories.Update(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? Id)
+        {
+            var category = _context.Categories.Find(Id);
+            if (category == null)
+                return NotFound();
+            else
+                return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? Id,Category model)
+        {
+            if(Id != null)
+            {
+                _context.Categories.Remove(model);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
         }
     }
 }
